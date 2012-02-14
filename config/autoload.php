e42e73d3363ab52e967649b07a842f6582f1e652
tehaon <kenny@pwnlog.com>
2012-02-13 22:14:54 -0600

    alpha upload.

---
+++ config/autoload.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *  Load required libraries
+ */
+
+ 
+$autoload['libraries']  = array('User');
+$autoload['model']      = array('user_model');
+$autoload['config']     = array('user');
---
+++ config/user.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//If auto_encrypt is enabled you must also enable CI Encryption library.
+$config['user']['auto_encrypt'] = 1;
+$config['user']['pw_salt']      = 'my-password_salt';
